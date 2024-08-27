cc37afffbaceac3587043d7787e0a4356f4be701
AiTrinh303 <trong.trinh@dci-student.org>
2024-08-27 14:01:30 +0200

    update path connect backend to frontent

--- .history/backend/index_20240827135939.js
+++ .history/backend/index_20240827135939.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'frontend', '/frontend/dist', '/frontend/dist/index.html'));
+		res.sendFile(path.resolve(__dirname, 'frontend', '/frontend/dist', 'index.html'));
 	});
 }
 
